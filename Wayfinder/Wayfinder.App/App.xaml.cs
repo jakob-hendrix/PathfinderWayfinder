7250c6b07c4c8087921ee63b3283dc5fad812bd3
Jakob Hendrix <jhendrix001@gmail.com>
2026-01-27 07:56:58 -0600

    Added new Wayfinder MAUI app
    Moved the old code to 'old' for now

---
+++ Wayfinder/Wayfinder.App/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Wayfinder.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Wayfinder.App" };
+        }
+    }
+}
